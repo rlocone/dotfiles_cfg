e0b65f57dd51607ab63f2b1faba6dceb171c38f7
rlocone <50329599+rlocone@users.noreply.github.com>
2021-03-05 22:18:22 -0500

    Update config.fish

--- .config/fish/conf.d/config.fish
+++ .config/fish/conf.d/config.fish
@@ -6,13 +6,56 @@ function ls
 end
 
 # Editor
-set -gx EDITOR vim
+set -gx EDITOR "vim"
+set TERM "xterm-256color"  
+
+# "bat" as manpager
+set -x MANPAGER "sh -c 'col -bx | bat -l man -p'" 
 
 # Ruby to path (For colorls)
 fish_add_path /home/rlocone/.local/share/gem/ruby/2.7.0/bin
 
 # Remove Greetiing (Sorry, it's annoying)
 set -U fish_greeting
+
+# bare git repo alias for dotfiles
+alias config="/usr/bin/git --git-dir=$HOME/dotfiles --work-tree=$HOME"
+
+# bat
+alias cat='bat'
+
+# Colorize grep output (good for log files)
+alias grep='grep --color=auto'
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+
+# Functions needed for !! and !$
+function __history_previous_command
+  switch (commandline -t)
+  case "!"
+    commandline -t $history[1]; commandline -f repaint
+  case "*"
+    commandline -i !
+  end
+end
+
+function __history_previous_command_arguments
+  switch (commandline -t)
+  case "!"
+    commandline -t ""
+    commandline -f history-token-search-backward
+  case "*"
+    commandline -i '$'
+  end
+end
+# The bindings for !! and !$
+if [ $fish_key_bindings = fish_vi_key_bindings ];
+  bind -Minsert ! __history_previous_command
+  bind -Minsert '$' __history_previous_command_arguments
+else
+  bind ! __history_previous_command
+  bind '$' __history_previous_command_arguments
+end
 
 # Dracula Theme
 set -l foreground f8f8f2
